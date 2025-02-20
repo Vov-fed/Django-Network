16a8ed0bb997a3eb2f15c581010205e97f3809ae
Vovan-fedoruk <vova.fedoruk@icloud.com>
2025-02-20 09:49:49 +0200

    modified:   myapp/serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -19,6 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
-            password=validated_data['password']
+            password=validated_data['password']  
+            
         )
         return user
