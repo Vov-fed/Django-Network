6376f4b492493634419171b929d1a095c72363c5
Vovan-fedoruk <vova.fedoruk@icloud.com>
2025-02-20 09:55:45 +0200

    modified:   myapp/__pycache__/serializers 	modified:   myapp/serializers.py 	modified:   myproject/__pycache__/settings 	modified:   myproject/settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8*ru(a0dhot9&$(f@)!r-n12c&*xju*so-(@*+u@ylta1pu_3e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['django-network.onrender.com', 'localhost']
 
 
 # Application definition
